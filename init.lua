38a05c91f9675f1166779121ee44bd6275cfda4e
tanktabox <tanktabox@gmail.com>
2024-02-23 18:31:23 +0900

    draculaのインストールとカラースキーマの設定

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ require("core.base")
 require("core.autocmds")
 require("core.options")
 require("core.keymaps")
-require("core.colorscheme")
 require("core.lazy")
+require("core.colorscheme") -- dracuraをlazy.vimでインストールしているためその後に読み込む
--- lua/core/base.lua
+++ lua/core/base.lua
@@ -9,3 +9,4 @@ vim.api.nvim_exec('language message en_US.UTF-8', true)
 -- 文字コードの設定
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8", "sjis"
+
